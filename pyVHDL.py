95063476acadb7427f372f4ba7267c517a90ccf6
DRubioG <35471722+DRubioG@users.noreply.github.com>
2020-08-31 18:20:15 +0200

    ee

--- pyVHDL.py
+++ pyVHDL.py
@@ -23,58 +23,71 @@ class pyVHDL:
     def list_ports(self,input, output, bits, MSB, LSB, invert, type):
         if invert==0:
             if bits ==0:
-                MSB_l=0
-                LSB_l=0
-                type="std_logic"
+                MSB_l=MSB
+                LSB_l=MSB
             elif bits>0 and MSB==0 and LSB==0:
                 LSB_l=0
                 MSB_l=bits-1
-                type="std_logic_vector"
             elif MSB>0:
                 MSB_l=MSB
                 LSB_l=MSB-bits
-                type="std_logic_vector"
                 if(LSB_l<0):
                     print("Error en LSB")
                     LSB_l=0
             elif LSB>0:
                 LSB_l=LSB
                 MSB_l=LSB+bits
-                type="std_logic_vector"
         elif invert==1:
             if bits ==0:
-                MSB_l=0
-                LSB_l=0
-                type="std_logic"
+                MSB_l=MSB
+                LSB_l=MSB
             elif bits>0 and MSB==0 and LSB==0:
                 LSB_l=bits-1
                 MSB_l=0
-                type="std_logic_vector"
             elif MSB>0:
                 MSB_l=MSB
                 LSB_l=MSB+bits
-                type="std_logic_vector"
             elif LSB>0:
                 LSB_l=LSB
                 MSB_l=LSB-bits
-                type="std_logic_vector"
                 if(MSB_l<0):
                     print("Error en LSB")
                     MSB_l=0
         output.append([input, type, MSB_l, LSB_l])
 
-    def port_in(self, port=None, bits=0, MSB=0, LSB=0, invert=0, type="std_logic"):
+    def port_in(self, port=None, bits=0, MSB=0, LSB=0, invert=0, type=None):
+        if type is None:
+            if bits==0:
+                type="std_logic"
+            elif bits>0:
+                type="std_logic_vector"
         self.list_ports(port, self.inPorts, bits, MSB, LSB, invert, type)
         
-    def port_out(self, port=None):
-        if port is not None:
-            self.outPorts.append(port)
+    def port_out(self, port=None, bits=0, MSB=0, LSB=0, invert=0, type="std_logic"):
+        self.list_ports(port, self.outPorts, bits, MSB, LSB, invert, type)
 
-    def signal(self, signal=None):
-        if signal is not None:
-            self.signals.append(signal)
+    def signal(self, signal=None, bits=0, MSB=0, LSB=0, invert=0, type="std_logic"):
+        self.list_ports(signal, self.signals, bits, MSB, LSB, invert, type)
 
     def constant(self, constant=None):
         if constant is not None:
             self.constants.append(constant)
     
+
+
+if __name__=="__main__":
+    v=pyVHDL("PWM_generator.vhd")
+    #v.li   brary("ieee") ###libreria por defecto
+    #v.use("std_logic_1164") ###libreria por defecto
+    v.use("numeric_std")
+    v.port_in("rst")
+    v.port_in("clk")
+    v.port_in("periodo",MSB=25, type="unsigned")
+    v.port_in("ciclo_trabajo", 23)
+    v.port_out("pwm")
+    v.port_out("final_pwm")
+    print(v.inPorts)
+    print(v.outPorts)
+    v.signal("senal_portadora", 23) 
+    v.signal("final_pwm_int")
+    print(v.signals)
